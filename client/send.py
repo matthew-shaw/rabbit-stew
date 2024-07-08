c28c0ff084ed0a46e4cc38eef42f9bf419f2cca0
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2024-07-08 15:48:53 +0100

    formatting

--- client/send.py
+++ client/send.py
@@ -4,8 +4,19 @@ if __name__ == "__main__":
     producer = Producer("amqps://exchange:5671")
     producer.declare_queue("greeting")
 
-    greetings = ["Hello", "Nǐn hǎo", "Namaste", "Hola", "Bonjour", "Asalaam alaikum", "Ciao", "Guten Tag", "Konnichiwa", "Hej"]
+    greetings = [
+        "Hello",
+        "Nǐn hǎo",
+        "Namaste",
+        "Hola",
+        "Bonjour",
+        "Asalaam alaikum",
+        "Ciao",
+        "Guten Tag",
+        "Konnichiwa",
+        "Hej",
+    ]
     for greeting in greetings:
         producer.send_message(exchange="", routing_key="greeting", body=greeting)
-    
+
     producer.close()
