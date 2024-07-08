27bd937bbb03a2861ccdcbd812cb7759e9c0af7a
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2024-07-08 14:40:10 +0100

    send many greetings

--- client/send.py
+++ client/send.py
@@ -3,5 +3,9 @@ from pika_client import Producer
 if __name__ == "__main__":
     send = Producer("amqps://exchange:5671")
     send.declare_queue("hello")
-    send.send_message(exchange="", routing_key="hello", body=b"Hello World!")
+
+    greetings = ["Hello", "Nǐn hǎo", "Namaste", "Hola", "Bonjour", "Asalaam alaikum", "Ciao", "Guten Tag", "Konnichiwa", "Hej"]
+    for greeting in greetings:
+        send.send_message(exchange="", routing_key="hello", body=greeting)
+    
     send.close()
