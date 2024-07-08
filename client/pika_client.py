4fc1b43ab535e61fc7525c9a47fc0082724abecf
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2024-07-08 12:58:33 +0100

    replace prints with logging

--- client/pika_client.py
+++ client/pika_client.py
@@ -1,15 +1,16 @@
+import logging
 import ssl
 
 import pika
+
+logging.basicConfig(level=logging.INFO)
 
 
 class PikaClient:
     def __init__(self, host):
         ssl_context = ssl.create_default_context(cafile="/ssl/ca_certificate.pem")
         ssl_context.verify_mode = ssl.CERT_REQUIRED
-        ssl_context.load_cert_chain(
-            "/ssl/client_exchange_certificate.pem", "/ssl/client_exchange_key.pem"
-        )
+        ssl_context.load_cert_chain("/ssl/client_exchange_certificate.pem", "/ssl/client_exchange_key.pem")
 
         parameters = pika.URLParameters(host)
         parameters.ssl_options = pika.SSLOptions(context=ssl_context)
@@ -18,7 +19,7 @@ class PikaClient:
         self.channel = self.connection.channel()
 
     def declare_queue(self, queue_name):
-        print(f"Trying to declare queue({queue_name})...")
+        logging.info(f"Trying to declare queue({queue_name})...")
         self.channel.queue_declare(queue=queue_name)
 
     def close(self):
@@ -30,19 +31,15 @@ class Producer(PikaClient):
     def send_message(self, exchange, routing_key, body):
         channel = self.connection.channel()
         channel.basic_publish(exchange=exchange, routing_key=routing_key, body=body)
-        print(
-            f"Sent message. Exchange: {exchange}, Routing Key: {routing_key}, Body: {body}"
-        )
+        logging.info(f"Sent message. Exchange: {exchange}, Routing Key: {routing_key}, Body: {body}")
 
 
 class Consumer(PikaClient):
     def consume_messages(self, queue):
         def callback(ch, method, properties, body):
-            print(" [x] Received %r" % body)
+            logging.info(" [x] Received %r" % body)
 
-        self.channel.basic_consume(
-            queue=queue, on_message_callback=callback, auto_ack=True
-        )
+        self.channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=True)
 
-        print(" [*] Waiting for messages. To exit press CTRL+C")
+        logging.info(" [*] Waiting for messages...")
         self.channel.start_consuming()
