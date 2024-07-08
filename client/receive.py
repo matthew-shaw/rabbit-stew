0f61804874f903f73e37e95655926a1f5dcf100c
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2024-07-08 14:47:25 +0100

    refactoring

--- client/receive.py
+++ client/receive.py
@@ -1,7 +1,7 @@
 from pika_client import Consumer
 
 if __name__ == "__main__":
-    receive = Consumer("amqps://exchange:5671")
-    receive.declare_queue("hello")
-    receive.consume_messages("hello")
-    receive.close()
+    consumer = Consumer("amqps://exchange:5671")
+    consumer.declare_queue("greeting")
+    consumer.consume_messages("greeting")
+    consumer.close()
--- client/send.py
+++ client/send.py
@@ -1,11 +1,11 @@
 from pika_client import Producer
 
 if __name__ == "__main__":
-    send = Producer("amqps://exchange:5671")
-    send.declare_queue("hello")
+    producer = Producer("amqps://exchange:5671")
+    producer.declare_queue("greeting")
 
     greetings = ["Hello", "Nǐn hǎo", "Namaste", "Hola", "Bonjour", "Asalaam alaikum", "Ciao", "Guten Tag", "Konnichiwa", "Hej"]
     for greeting in greetings:
-        send.send_message(exchange="", routing_key="hello", body=greeting)
+        producer.send_message(exchange="", routing_key="greeting", body=greeting)
     
-    send.close()
+    producer.close()
