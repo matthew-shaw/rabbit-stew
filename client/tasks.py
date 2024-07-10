fb3929abaa9cc70f57b900955c67e9cd203cabca
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2024-07-10 12:58:49 +0100

    add comments

--- client/tasks.py
+++ client/tasks.py
@@ -3,9 +3,13 @@ import os
 from rabbit_client import Producer
 
 if __name__ == "__main__":
+    # Create a producer client instance
     producer = Producer(host=os.environ.get("RABBIT_HOST", ""))
+
+    # Create a direct exchange
     producer.declare_exchange(name="tasks", type="direct")
 
+    # Make up some messages
     tasks = [
         "Clean the bathroom",
         "Clean the bedroom",
@@ -29,6 +33,8 @@ if __name__ == "__main__":
         "Water the plants",
     ]
     for task in tasks:
+        # Publish messages to the exchange
         producer.publish(exchange="tasks", routing_key="tasks", message=task)
 
+    # Close the connection
     producer.close()
