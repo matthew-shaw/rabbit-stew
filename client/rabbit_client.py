ca457797d06dee3e0275eec4f19242cd6ef44361
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2024-07-10 13:54:50 +0100

    consistent logging

--- client/rabbit_client.py
+++ client/rabbit_client.py
@@ -22,21 +22,23 @@ class RabbitClient:
 
         self.connection = pika.BlockingConnection(parameters)
         self.channel = self.connection.channel()
+        logging.info(f"Established secure connection to: '{host}'")
 
     def declare_exchange(self, name: str, type: Literal["direct", "fanout", "topic", "headers"]) -> None:
         """Creates a new exchange with a given name and type."""
-        logging.info(f"Trying to declare exchange({name})...")
         self.channel.exchange_declare(exchange=name, exchange_type=type)
+        logging.info(f"Declared exchange: '{name}'...")
 
     def declare_queue(self, name: str, exclusive=False) -> None:
         """Creates a new durable queue with a given name."""
-        logging.info(f"Trying to declare queue({name})...")
         self.channel.queue_declare(queue=name, exclusive=exclusive, durable=True)
+        logging.info(f"Declared queue: '{name}'...")
 
     def close(self) -> None:
         """Closes the channel and connection."""
         self.channel.close()
         self.connection.close()
+        logging.info(f"Closed connection")
 
 
 class Producer(RabbitClient):
@@ -49,27 +51,27 @@ class Producer(RabbitClient):
             body=message,
             properties=pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent),
         )
-        logging.info(f"Sent message. Exchange: {exchange}, Routing Key: {routing_key}, Body: {message}")
+        logging.info(f"Sent message '{message}' to exchange '{exchange}' with routing key '{routing_key}'")
 
 
 class Consumer(RabbitClient):
     """Binds an existing queue to an exchange."""
 
     def bind_queue(self, exchange: str, queue: str) -> None:
-        logging.info(f"Trying to bind queue({queue}) to exchange({exchange})...")
         self.channel.queue_bind(exchange=exchange, queue=queue)
+        logging.info(f"Bound queue '{queue}' to exchange '{exchange}'...")
 
     def consume_messages(self, queue: str) -> None:
         """Starts consuming messages from a queue."""
 
         def callback(ch, method, properties, body):
-            logging.info(f"[x] Received {body}")
+            logging.info(f"Received message: '{body}'")
             time.sleep(2)  # Sleep to simulate real message processing happening here
             ch.basic_ack(delivery_tag=method.delivery_tag)
-            logging.info(f"[x] Acknowledged {body}")
+            logging.info(f"Acknowledged message: '{body}'")
 
         self.channel.basic_qos(prefetch_count=1)
         self.channel.basic_consume(queue=queue, on_message_callback=callback)
 
-        logging.info("[*] Waiting for messages...")
+        logging.info(f"Waiting for messages on queue: '{queue}'...")
         self.channel.start_consuming()
